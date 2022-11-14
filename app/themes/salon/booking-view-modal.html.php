effa74aedec5aab6c01d9bce9ae1c947f18d0fb0
Xavier <xavier.tnc@gmail.com>
2022-11-14 12:12:13 +0200

    Salon Allure v1.3.0 - FT - themes/salon/booking-modal.html.php: Rename modal file + Add `booking-view-model.html.php`

---
+++ app/themes/salon/booking-view-modal.html.php
@@ -0,0 +1,15 @@
+<!-- Appointment Modal -->
+<section id="booking-view-modal" class="modal">
+  <div class="modal-inner">
+    <header>
+      <h3>Appointment</h3>
+      <a class="modal-close" onclick="F1.Modal.close(this, event)">Close X</a>
+    </header>
+    <div class="modal-body"></div>
+    <footer>
+      <p style="text-align:right">
+        <button class="button btn-edit" type="button" onclick="F1.onEditAppointment(event)">Edit</button>
+      </p>
+    </footer>
+  </div>
+</section>
