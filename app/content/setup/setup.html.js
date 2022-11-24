66ea231e6a0c533eb7303f7272ab177688bd0f04
Xavier <xavier.tnc@gmail.com>
2022-11-24 20:46:54 +0200

    Salon Allure v2.2.0 - FT - content/setup: Properly initialize the new setup page.

---
+++ app/content/setup/setup.html.js
@@ -0,0 +1,6 @@
+/* View Specific JS */
+F1.deferred.push(function initPage() {
+
+  console.log( '[Salon Setup Page] Says Hi!' );
+
+});
---
+++ app/content/setup/setup.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * ./app/content/setup/setup.php
+ * 
+ * Setup page controller - 22 Nov 2022
+ *
+ * @author C. Moller <xavier.tnc@gmail.com>
+ * 
+ * @version 1.0.0 - 22 Nov 2022
+ * 
+ */
+
+$view->theme = 'salon';
+$view->title = 'Setup';
+
+
+// -----------
+// --- GET ---
+// -----------
+
+
+include $view->getFile();
