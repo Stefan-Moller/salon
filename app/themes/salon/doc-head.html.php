9cdfb0858bbfe73b14e06634c3c06bb9ad84f02c
Xavier <xavier.tnc@gmail.com>
2022-11-28 15:12:15 +0200

    Salon Allure v2.5.2 - DEV - themes/salon/doc-head.html.php: Rename `getStylesFile` to `getInlineStylesFile`. Remove "css/" prefix from style include href's.

--- app/themes/salon/doc-head.html.php
+++ app/themes/salon/doc-head.html.php
@@ -6,11 +6,11 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title><?=$app->title?> - <?=$view->title?></title>
   <base href="<?=$app->baseUri?>">
-  <?php foreach( $view->styles as $styleFile ): ?>
-  <link rel="stylesheet" type="text/css" href="css/<?=$styleFile?>">
+  <?php foreach( $view->styles as $styleFileRef ): ?>
+  <link rel="stylesheet" type="text/css" href="<?=$styleFileRef?>">
   <?php endforeach; ?>
   <link rel="stylesheet" type="text/css" href="css/salon-theme.css">
-  <style><?php include $view->getStylesFile(); ?></style>
+  <style><?php include $view->getInlineStylesFile(); ?></style>
   <script src="js/vendors/f1js/f1.js"></script>
   <script>F1.DEBUG = <?=__DEBUG_ON__?>; F1.page = '<?=$view->name?>';</script>
   <script>document.documentElement.style.setProperty('--scrollbar-width', (window.innerWidth - document.documentElement.offsetWidth) + 'px');</script>
